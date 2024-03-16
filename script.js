20d6c5f35520997b104806997c8abfd1671f9bb9
Divyansh <furanthiopene@gmail.com>
2024-03-16 22:11:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let data1 ={
+  "Name" : document.getElementById("Name").value,
+  "Email" :document.getElementById("email").value,
+  "Message":document.getElementById("message").value
+};
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 2000);
+  loaded();
+};
+
+function loaded(){
+  console.log("loaded")
+  document.getElementById("submitbtn").addEventListener("click",()=>{
+    console.log("clicked");
+    postData();
+  })
+}
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 500);
+});
+
+
+
+function postData(){
+fetch('https://nlgrjwhxbk.execute-api.us-east-1.amazonaws.com/dev/',{
+  Method: 'POST',
+  Headers: {
+    Accept: 'application.json',
+    'Content-Type': 'application/json',
+    Access-Control-Allow-Origin: "*"
+  },
+  Body: JSON.stringify(data1),
+  Cache: 'default'
+
+})
+      .then(response => response.json())
+      .then(json => console.log(json))
+};
+
+// arn:aws:iam::239678915342:role/API_S3_proxy_policy
