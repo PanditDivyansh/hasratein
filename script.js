011d7fba3b1f94077c0b20e63997d619e3c8cdf1
PanditDivyansh <92201174+PanditDivyansh@users.noreply.github.com>
2024-03-17 20:02:24 +0530

    script.js

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ fetch('https://snbxzuu3e8.execute-api.us-east-1.amazonaws.com/dev/POST/',{
   Method: 'POST',
   mode: 'cors',
   Headers: {
+    "Content-type": "application/json; charset=UTF-8",
     "Accept": "application.json",
     "Authorization" : "None",
     "Access-Control-Allow-Origin": "*",
