5152a067c037a003a8a612443d0776e5093c45ca
PanditDivyansh <92201174+PanditDivyansh@users.noreply.github.com>
2024-04-05 21:51:45 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// let data1 ={
-//   "Name" : document.getElementById("Name").value,
-//   "Email" :document.getElementById("email").value,
-//   "Message":document.getElementById("message").value
-// };
-
 window.onload = () => {
   setTimeout(() => {
     document.querySelector("body").classList.add("display");
@@ -32,22 +26,6 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
 
 
 function postData(){
-// fetch('https://snbxzuu3e8.execute-api.us-east-1.amazonaws.com/dev/',{
-//   Method: 'POST',
-//   mode: 'cors',
-//   Headers: {
-//     "Content-type": "application/json; charset=UTF-8",
-//     "Accept": "application.json",
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Request-Method": "OPTIONS,POST",
-//     "Access-Control-Request-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
-//   },
-//   Body: JSON.stringify(data1),
-//   Cache: 'default'
-
-// })
-//       .then(response => response.json())
-//       .then(json => console.log(json))
 
   const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
@@ -71,10 +49,10 @@ fetch("https://snbxzuu3e8.execute-api.us-east-1.amazonaws.com/dev", requestOptio
   .catch((error) => console.error(error));
 };
 
-fetch("https://tsjpxpxa8e.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
-};
+// fetch("https://tsjpxpxa8e.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error));
+// };
 
 // arn:aws:iam::239678915342:role/API_S3_proxy_policy
